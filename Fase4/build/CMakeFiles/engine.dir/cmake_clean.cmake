36d4f588b233d99e70da98fafbb2a354d7a97cda
Mariana Marques <marianamarquesworkplace@gmail.com>
2023-05-20 20:05:07 +0100

    init fase4, normals done, light done but not correct

---
+++ Fase4/build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/engine/camera.cpp.o"
+  "CMakeFiles/engine.dir/engine/camera.cpp.o.d"
+  "CMakeFiles/engine.dir/engine/engine.cpp.o"
+  "CMakeFiles/engine.dir/engine/engine.cpp.o.d"
+  "CMakeFiles/engine.dir/engine/group.cpp.o"
+  "CMakeFiles/engine.dir/engine/group.cpp.o.d"
+  "CMakeFiles/engine.dir/engine/light.cpp.o"
+  "CMakeFiles/engine.dir/engine/light.cpp.o.d"
+  "CMakeFiles/engine.dir/engine/window.cpp.o"
+  "CMakeFiles/engine.dir/engine/window.cpp.o.d"
+  "CMakeFiles/engine.dir/utils/tinyxml2.cpp.o"
+  "CMakeFiles/engine.dir/utils/tinyxml2.cpp.o.d"
+  "engine"
+  "engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
